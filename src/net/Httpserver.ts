ac15645b48c985f866ee3444f050fd45d9fad289
chenxuan <523764720@qq.com>
2019-11-25 17:06:45 +0800

    勉强有httpserver 但是代码方面很不符合我的标准

--- src/net/Httpserver.ts
+++ src/net/Httpserver.ts
@@ -1,24 +1,75 @@
-const HTTPSERVER = require("@awesomeeng/awesome-server")
+import http_shutdown from "http-shutdown";
+const HTTPSERVER =require('node-http-server');
 
-export class Httpserver
+export class Httpserver extends HTTPSERVER.Server
 {
-    server: any
+    is_running = false
+    is_stopping = false
 
     async start()
     {
-        let server = new HTTPSERVER()
-        this.server = server
-        server.addHTTPServer({
-            hostname: "localhost",
-            port: 8000
-        });
-        server.route("GET","/terminal_show",this.on_terminal_show)
-        await server.start()
+        const config = new HTTPSERVER.Config;
+        
+        config.port = 8000;
+        
+        await new Promise ((succ) =>
+        {
+            setTimeout(() =>
+            {
+                this.deploy(config);
+            })
+            this.is_running = true
+            setInterval(() =>
+            {
+                if(!this.is_running)
+                {
+                    succ()
+                }
+            }, 1e3)
+
+        })
+
     }
 
-    async on_terminal_show(path: any,request: any,response: any)
+    onRawRequest(request: { uri: any; headers: any; connection: { end: () => void; destroy: any; }; },response: any,serve: (arg0: any, arg1: any, arg2: string) => void){
+     
+        serve(
+            request,
+            response,
+            JSON.stringify(
+                {
+                    uri:request.uri,
+                    headers:request.headers
+                }
+            )
+        );
+        console.log("income");
+        
+        if(this.is_stopping)
+        {
+            response.end(); //close the response
+            request.connection.end(); //close the socket
+            request.connection.destroy; //close it really
+        }
+
+        this.close_now()
+        
+        return true;
+    }
+
+    close_now()
     {
-        console.log("stop");
-        this.server.stop()
+        if(this.is_stopping)
+        {
+            return
+        }
+        this.is_stopping = true
+        this.server.on("close", () =>
+        {
+            console.log("shutdown");
+            this.is_stopping = false
+            this.is_running = false
+        })
+        this.server.close()
     }
 }
--- test/Httpserver.ts
+++ test/Httpserver.ts
@@ -6,5 +6,11 @@ describe("Httperserver", () =>
     it("# 基础运行", async () =>
     {
         await new Httpserver().start()
+    }).timeout(5 * 60e3)
+
+    it("# 基础运行2", async () =>
+    {
+        console.log(1111);
     })
+
 })
