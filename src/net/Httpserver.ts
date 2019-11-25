fe81bfdff2391d3373c2f1a59d47d4c2904d6597
chenxuan <523764720@qq.com>
2019-11-25 17:12:04 +0800

    移除多余的代码, 从零开始

--- src/net/Httpserver.ts
+++ src/net/Httpserver.ts
@@ -1,75 +1,16 @@
-import http_shutdown from "http-shutdown";
-const HTTPSERVER =require('node-http-server');
-
-export class Httpserver extends HTTPSERVER.Server
+export class Httpserver
 {
     is_running = false
     is_stopping = false
 
     async start()
     {
-        const config = new HTTPSERVER.Config;
         
-        config.port = 8000;
-        
-        await new Promise ((succ) =>
-        {
-            setTimeout(() =>
-            {
-                this.deploy(config);
-            })
-            this.is_running = true
-            setInterval(() =>
-            {
-                if(!this.is_running)
-                {
-                    succ()
-                }
-            }, 1e3)
-
-        })
-
     }
 
-    onRawRequest(request: { uri: any; headers: any; connection: { end: () => void; destroy: any; }; },response: any,serve: (arg0: any, arg1: any, arg2: string) => void){
-     
-        serve(
-            request,
-            response,
-            JSON.stringify(
-                {
-                    uri:request.uri,
-                    headers:request.headers
-                }
-            )
-        );
-        console.log("income");
-        
-        if(this.is_stopping)
-        {
-            response.end(); //close the response
-            request.connection.end(); //close the socket
-            request.connection.destroy; //close it really
-        }
-
-        this.close_now()
-        
-        return true;
-    }
 
     close_now()
     {
-        if(this.is_stopping)
-        {
-            return
-        }
-        this.is_stopping = true
-        this.server.on("close", () =>
-        {
-            console.log("shutdown");
-            this.is_stopping = false
-            this.is_running = false
-        })
-        this.server.close()
+        
     }
 }
