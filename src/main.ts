2c431e1345e0d39b8d79e21b350a7cc958eacd55
chenxuan <523764720@qq.com>
2019-11-27 10:55:35 +0800

    首个感觉可以跑的版本

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { Httpserver } from "./net/Httpserver";
+import { Terminal_manager } from "./terminal/Terminal_manager";
 
 (async ()=>
 {
     console.log("server start");
     
-    await new Httpserver().start()
+    await new Httpserver(new Terminal_manager()).start()
 })()
--- src/net/Httpserver.ts
+++ src/net/Httpserver.ts
@@ -1,21 +1,62 @@
 import Koa from "koa";
 import Router from "koa-router";
+import { Terminal_manager } from "./../terminal/Terminal_manager";
 
 export class Httpserver
 {
-    is_running = false
-    is_stopping = false
+    t_m: Terminal_manager
+    constructor(t_m: Terminal_manager)
+    {
+        this.t_m = t_m
+    }
+
+    async web_all(ctx: Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>)
+    {
+        ctx.body = this.t_m.all()
+    }
+
+    async web_create(ctx: Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>)
+    {
+        ctx.body = this.t_m.create()
+    }
+
+    async web_close(ctx: Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>)
+    {
+        ctx.body = this.t_m.close(ctx.params.id)
+    }
+
+    async web_run(ctx: Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>)
+    {
+        ctx.body = this.t_m.run(ctx.params.id, ctx.params.cmd)
+    }
+
+    async web_result(ctx: Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>)
+    {
+        ctx.type = "text/html"
+        ctx.body = this.t_m.result(ctx.params.id)
+    }
+
+    router_init()
+    {
+        let router = new Router ()
+        
+        router.get('/all', this.web_all.bind(this))
+        
+        router.get('/create', this.web_create.bind(this))
+        
+        router.get('/close/:id', this.web_close.bind(this))
+        
+        router.get('/run/:id/:cmd', this.web_run.bind(this))
+        
+        router.get('/result/:id', this.web_result.bind(this))
+
+        return router
+    }
 
     async start()
     {
         let app = new Koa()
-        let router = new Router ()
-
-        router.get('/aaa', async (ctx,next) => {
-            await next()
-            ctx.body = 'Hello Router';
-            console.log(ctx.query);
-        })
+        let router = this.router_init()
 
         app.use(router.routes()).use(router.allowedMethods())
 
